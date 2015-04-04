519c9f7e91229bacea35e66445f2df793447e76a
Andy Sarroff <sarroff@cs.dartmouth.edu>
2015-04-04 19:04:13 -0400

    Tried to add template for complex float

--- include/caffe/common.hpp
+++ include/caffe/common.hpp
@@ -18,6 +18,8 @@
 
 #include "caffe/util/device_alternate.hpp"
 
+#include <complex>
+
 // gflags 2.1 issue: namespace google was changed to gflags without warning.
 // Luckily we will be able to use GFLAGS_GFAGS_H_ to detect if it is version
 // 2.1. If yes, we will add a temporary solution to redirect the namespace.
